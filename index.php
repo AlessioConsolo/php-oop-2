588164b860f5bd88fe2091c4ae30e47584d6e0e3
AlessioConsolo <alessioconsolo3@gmail.com>
2024-09-18 18:49:29 +0200

    [UPD] Added folder "images" and style.css, and created the class Product on index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Negozio Online per Animali</title>
+</head>
+<body>
+    <h1>Negozio Online per Animali</h1>
+    
+    <?php
+
+    class Product {
+        protected $name;
+        protected $price;
+        protected $image;
+        protected $category;
+        protected $type;
+
+        public function __construct($name, $price, $image, Category $category, $type) {
+            $this->name = $name;
+            $this->price = $price;
+            $this->image = $image;
+            $this->category = $category;
+            $this->type = $type;
+        }
+
+        public function getName() {
+            return $this->name;
+        }
+
+        public function getPrice() {
+            return $this->price;
+        }
+
+        public function getImage() {
+            return $this->image;
+        }
+
+        public function getCategory() {
+            return $this->category;
+        }
+
+        public function getType() {
+            return $this->type;
+        }
+    }
+    ?>
+</body>
+</html>
