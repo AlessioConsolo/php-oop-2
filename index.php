55527d3b7952df4356ae63778dabeeb9b82850e4
AlessioConsolo <alessioconsolo3@gmail.com>
2024-09-18 18:54:17 +0200

    [UPD] Added class Category

--- index.php
+++ index.php
@@ -85,6 +85,24 @@
         }
     }
 
+    class Category {
+        private $name;
+        private $icon;
+
+        public function __construct($name, $icon) {
+            $this->name = $name;
+            $this->icon = $icon;
+        }
+
+        public function getName() {
+            return $this->name;
+        }
+
+        public function getIcon() {
+            return $this->icon;
+        }
+    }
+
 
 ?>
 </body>
